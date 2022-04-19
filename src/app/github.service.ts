1bdb0e173ae32a068a31206f1aab6b973c3476e2
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-19 05:59:42 +0300

    added an handleErrors method

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,7 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { count } from 'console';
 import { CLIENT_ID , CLIENT_SECRET } from 'credentials/GithubCred';
+import { throwError } from 'rxjs';
 import { retry } from 'rxjs';
 
 
@@ -14,8 +16,23 @@ export class GithubService {
   public getProfile(){
     let dataURL = 'https://api.github.com/users/${searchQuery}?client_id= ${CLIENT_ID}%client_secret= ${CLIENT_SECRET}';
   return this.httpClient.get(dataURL).pipe(
-    retry(count:1),
+    retry(count: 1),
   );
   }
+
+  public handleErrors(error:HttpErrorResponse){
+    let errorMessage:string;
+    if(error.error instanceof ErrorEvent){
+      errorMessage = 'MESSAGE: ${error.error.message}';
+
+    }
+    else{
+      errorMessage = 'STATUS : $(error.status)MESSAGE:${error.message}';
+
+    }
+    return throwError(errorMessage);
+
+  }
 }
+
 
