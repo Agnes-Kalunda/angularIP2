dfaa9f98524da576274b7b6ac5c6692cfc679305
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-19 06:04:42 +0300

    edited service.ts

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { count } from 'console';
 import { CLIENT_ID , CLIENT_SECRET } from 'credentials/GithubCred';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs';
 import { throwError } from 'rxjs';
 import { retry } from 'rxjs';
 
@@ -13,10 +15,12 @@ import { retry } from 'rxjs';
 export class GithubService {
 
   constructor(private httpClient:HttpClient) { }
-  public getProfile(){
+  public getProfile(_searchQuery: any):Observable<any
+  >{
     let dataURL = 'https://api.github.com/users/${searchQuery}?client_id= ${CLIENT_ID}%client_secret= ${CLIENT_SECRET}';
   return this.httpClient.get(dataURL).pipe(
     retry(count: 1),
+    catchError(this.handleErrors)
   );
   }
 
