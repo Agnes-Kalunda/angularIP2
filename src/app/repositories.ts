56bd00dbfac723d251ce4a8a00abd70dc9a58abd
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-04-22 03:46:29 +0300

    added getUser() in user service

--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -1,2 +1,7 @@
 export class Repositories {
+    constructor(  public name: string,  
+        public description: string, 
+        public created_at:Date,
+        public html_url: string,){
+        }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  username:string;
+  user!:User;
+  repos!:any;
+
+  constructor(private http:HttpClient) { 
+    this.username ='Agnes-Kalunda';
+}
+getUser(){
+  return this.http.get('https://api.github.com/users/' + this.username)
+  .pipe(map(result => result));
+}
 }
